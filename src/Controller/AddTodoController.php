845d8604391d3f419f160cef89aa3b19345162aa
LucasSanchez82 <jiraya1008@gmail.com>
2025-01-18 00:09:35 +0100

    feat: addTodo

--- src/Controller/AddTodoController.php
+++ src/Controller/AddTodoController.php
@@ -2,17 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\Todo;
+use App\Form\TodoType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class AddTodoController extends AbstractController
 {
-    #[Route('/add/todo', name: 'app_add_todo')]
-    public function index(): Response
+    #[Route('/add/todo', name: 'add_todo')]
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $form = $this->createForm(TodoType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $todoToAdd = new Todo();
+            $todoToAdd->setTitle($data->getTitle());
+            $todoToAdd->setDescription($data->getDescription());
+            $todoToAdd->setFinished($data->isFinished());
+
+            $entityManager->persist($todoToAdd);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_home');
+        }
         return $this->render('add_todo/index.html.twig', [
-            'controller_name' => 'AddTodoController',
+            'form' => $form,
         ]);
     }
 }
--- src/Form/TodoType.php
+++ src/Form/TodoType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Todo;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,6 +16,7 @@ class TodoType extends AbstractType
             ->add('title')
             ->add('description')
             ->add('finished')
+            ->add('submit', SubmitType::class)
         ;
     }
 
