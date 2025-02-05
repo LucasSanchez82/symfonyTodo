b078c245c0e7720977a14c943a34d16fbb80d969
LucasSanchez82 <08.lucas.sanchez@gmail.com>
2025-02-05 14:38:28 +0100

    feat: add forms and templates for User and Category management

--- src/Form/TodoType.php
+++ src/Form/TodoType.php
@@ -2,7 +2,10 @@
 
 namespace App\Form;
 
+use App\Entity\Category;
 use App\Entity\Todo;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -16,7 +19,18 @@ class TodoType extends AbstractType
             ->add('title')
             ->add('description')
             ->add('finished')
-            ->add('category')
+            ->add('todos', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => function (User $user) {
+                    return $user->getFirstname() . ' ' . $user->getLastname();
+                },
+                'multiple' => true,
+                'expanded' => true
+            ])
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name',
+            ])
             ->add('submit', SubmitType::class)
         ;
     }
